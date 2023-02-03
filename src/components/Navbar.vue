17df6241f7a735ea998bcff4c334b4cd3d851ce0
Azharmaulanadzakki <dzakkimaulana1218@gmail.com>
2023-02-03 10:01:33 +0700

    feat:tailwind&login

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="relative px-4 py-10 flex justify-between items-center bg-white shadow-lg">
+        <ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
+            <li><a class="text-sm text-gray-600 hover:text-white font-['Poppins'] hover:bg-gray-700 py-2 px-6 rounded-xl" href="http://localhost:5173/">Home</a></li>
+            <li class="text-gray-300">
+            </li>
+            <li><a class="text-sm text-gray-600  hover:text-white font-['Poppins']  hover:bg-gray-700 py-2 px-6 rounded-xl" href="http://localhost:5173/about">About Us</a></li>
+            <li class="text-gray-300">
+            </li>
+            <li><a class="text-sm text-gray-600 hover:text-white font-['Poppins']  hover:bg-gray-700 py-2 px-6 rounded-xl" href="#">Resto</a></li>
+            <li class="text-gray-300">
+            </li>
+            <li><a class="text-sm text-gray-600 hover:text-white font-['Poppins']  hover:bg-gray-700 py-2 px-6 rounded-xl" href="#">Media Social</a></li>
+        </ul>
+    </nav>
+</template>
+ 
