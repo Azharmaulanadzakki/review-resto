b3a2095cc7aa513e280f404de06e48fc096d1cfa
Azharmaulanadzakki <dzakkimaulana1218@gmail.com>
2023-02-03 10:23:18 +0700

    feat:navbar & footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
+
 import { RouterView } from "vue-router";
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
 </script>
 
 <template>
   <div class="box-border antialiased bg-gray-200">
     <Navbar />
     <RouterView /> 
+    <Footer />
   </div>
 </template>
