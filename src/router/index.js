cd3d5e8cffbbbc0c02ba0b50657132ddf662650c
Azharmaulanadzakki <dzakkimaulana1218@gmail.com>
2023-02-09 11:35:29 +0700

    feat:detail resto

--- src/router/index.js
+++ src/router/index.js
@@ -4,23 +4,22 @@ import LoginView from "../views/LoginView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
     {
       path: "/",
       name: "login",
       component: LoginView,
     },
-
     {
-      path: "/home",
-      name: "home",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/HomeView.vue"),
+      path: "/restos",
+      name: "restos",
+      component: () => import("../views/RestoView.vue"),
     },
-
+    {
+      path: "/restos/:id",
+      name: "restos-show",
+      component: () => import("../views/RestoDetailView.vue"),
+    },
   ],
-}); 
+});
 
 export default router;
